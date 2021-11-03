879987ae4d6dce941cae82f7fae1009de61b3220
Anmay Gupta <anmaygupta1@gmail.com>
2021-11-02 19:04:29 -0500

    got dominoes and forces working

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Engine = Matter.Engine;
 const Render = Matter.Render;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
+const Body = Matter.Body;
 
 const drawBody = Helpers.drawBody;
 
@@ -9,26 +10,24 @@ let engine;
 
 let allObjectsArray = [];
 let dominoesArray = [];
-let numDominoes = 13;
-let boxA;
-let boxB;
+let numDominoes = screen.availWidth/55;
 let ground;
 
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(screen.availWidth, screen.availHeight);
 
   // create an engine
   engine = Engine.create();
 
   for (let index = 0; index < numDominoes; index++) {
-      var rectangle = Bodies.rectangle(100 + index*50, 400, 10, 80);
+      var rectangle = Bodies.rectangle(100 + index*40, 350, 15, 135, {
+          frictionAir: 0.005,
+      });
       allObjectsArray.push(rectangle);
       dominoesArray.push(rectangle);
   }
 
-  boxA = Bodies.rectangle(400, 400, 10, 80);
-  boxB = Bodies.rectangle(300, 400, 10, 80);
-  ground = Bodies.rectangle(400, 500, 810, 10, {
+  ground = Bodies.rectangle(screen.availWidth/2, 500, screen.availWidth, 10, {
     isStatic: true, 
     // angle: Math.PI * 0.06
   });
@@ -55,4 +54,11 @@ function draw() {
 
   fill(128);
   drawBody(ground);
+}
+
+document.getElementById("button").onclick = force;
+
+function force() {
+    console.log("Here");
+    Body.applyForce( dominoesArray[0], {x: dominoesArray[0].position.x, y: dominoesArray[0].position.y - 10}, {x: 0.025, y: 0});
 }
